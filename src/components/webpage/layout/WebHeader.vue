e8f77bc58a3149092cec1f86153031483929e733
Diego Reyes <diegorele13@gmail.com>
2023-07-25 20:07:08 -0500

    set: header

--- src/components/webpage/layout/WebHeader.vue
+++ src/components/webpage/layout/WebHeader.vue
@@ -1,15 +1,14 @@
 <template>
-  <header>
-    <p>
-      Eso es un header
-    </p>
+  <header class="header">
+    <img src="@/assets/logo.png" alt="Predix, predicciones deportivas">
   </header>
 </template>
 
 <style lang="scss">
-header {
-  p {
-    color: blue;
-  }
+.header {
+  padding: 24px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
