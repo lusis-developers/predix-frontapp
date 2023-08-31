60aad9edad7e260d39ee5839502a9915d4b965e4
Luis Alberto Reyes Lema <lualreye@Luiss-MacBook-Pro.local>
2023-08-31 14:02:45 -0500

    refactor: scafolding

--- src/views/InternalApp/ThePlans/ThePlans.vue
+++ src/views/InternalApp/ThePlans/ThePlans.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+import usePlanStore from '@/stores/PlansStore';
+import { onMounted } from 'vue';
+
+const planStore = usePlanStore();
+
+onMounted(async () => {
+  await planStore.getPlans();
+})
+</script>
+
 <template>
   <div>
     esto son los planes
