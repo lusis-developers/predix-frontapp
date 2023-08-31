5f0ad34d272cdd6b6603dee460158954a46f6e29
Diego Reyes <diegorele13@gmail.com>
2023-08-31 17:22:23 -0500

    done: plan card

---
+++ src/views/InternalApp/ThePlans/components/ThePlan.vue
@@ -0,0 +1,83 @@
+<script setup lang="ts">
+
+const props = defineProps({
+  image: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  }
+})
+
+</script>
+
+<template>
+  <div class="card">
+    <img
+      class="card-image" 
+      :src="image" :alt="title">
+    <p class="card-title">
+      {{title}} 
+    </p>
+    <p class="card-price">
+      ${{price}}
+    </p>
+    <CrushButton
+      class="card-button"
+      text="Editar Plan"
+      primary="primary"/>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+$gap-padding-space: 12px;
+
+.card {
+  margin-top: 24px;
+  display: flex;
+  gap: $gap-padding-space;
+  flex-direction: column;
+  width: 100%;
+  padding: 8px;
+  padding-bottom: $gap-padding-space * 2;
+  border: 1px solid $white;
+  border-radius: 8px;
+  @media (min-width: $tablet-lower-breakpoint) {
+    width: 45%;
+  }
+  &-image {
+    width: 100%;
+    height: 104px;
+    object-fit: cover;
+    border-radius: 8px;
+    overflow: hidden;
+    object-position: center top;
+  }
+  &-title {
+    font-family: $font-secondary;
+    text-overflow: ellipsis;
+    color: $white;
+    font-size: $body-font-size;
+    font-weight: 700;
+  }
+  &-price {
+    font-size: $body-font-size;
+    font-family: $font-secondary;
+  }
+  &-button {
+    background-color: $green;
+    margin: 0 auto;
+    width: 80%;
+    &:hover {
+      background-color: $light-green;
+      border: none;
+    }
+  }
+}
+</style>
