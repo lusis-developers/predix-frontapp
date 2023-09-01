f1cc1aea5188917865d15533f74cd2b575887636
Diego Reyes <diegorele13@gmail.com>
2023-08-31 19:34:24 -0500

    add: component

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -2,3 +2,4 @@ declare module  '@nabux-crush/crush-text-field';
 declare module '@nabux-crush/crush-text-area';
 declare module '@nabux-crush/crush-button';
 declare module  '@nabux-crush/crush-alert';
+declare module '@nabux-crush/crush-upload';
