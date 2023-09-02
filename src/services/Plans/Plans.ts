2c59b8daf7ecf216011be6a2fe2f59d2ab65d88d
Diego Reyes <diegorele13@gmail.com>
2023-09-02 15:00:25 -0500

    add: function uploadImage & createPlan

--- src/services/Plans/Plans.ts
+++ src/services/Plans/Plans.ts
@@ -1,9 +1,19 @@
 import APIBase from '../Base';
-import type { Plan } from '../../typings/PlanTypes';
+import type { Plan, ImageResponse } from '../../typings/PlanTypes';
 
 class APIPlans extends APIBase {
   async getPlans(): Promise<Plan[]> {
     return this.get<Plan[]>('plans');
+  }
+
+  async uploadImage(imageFile: string): Promise <ImageResponse> {
+    const formData = new FormData();
+    formData.append('planImage', imageFile);
+    return this.post<ImageResponse>('planImage', formData);
+  }
+
+  async createPlan(planData: Plan): Promise<Plan> {
+    return this.post<Plan>('plan', planData)
   }
 }
 
