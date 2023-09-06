f8b8949047bba56b935ac13ae67db63531e30af4
Diego Reyes <diegorele13@gmail.com>
2023-09-06 13:24:16 -0500

    fix: padding

--- src/components/web/layout/WebHeader.vue
+++ src/components/web/layout/WebHeader.vue
@@ -68,7 +68,7 @@ const buttons = [
     }
     button {
       border-radius: 8px;
-      padding: 12px;
+      padding: 12px 16px;
       color: $white;
       background: none;
       border: none;
