595e2c081cea465a58f8b392568c513deed917c5
Diego Reyes <diegorele13@gmail.com>
2023-09-06 14:04:57 -0500

    set: index

---
+++ src/views/Web/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import TheHero from './components/TheHero.vue';
+</script>
+
+<template>
+  <div class="container-hero">
+    <TheHero/>
+  </div>
+</template>
+
+<style lang="scss" scoepd>
+.container {
+  &-hero {
+    display: flex;
+    justify-content: center;
+  }
+}
+</style>
