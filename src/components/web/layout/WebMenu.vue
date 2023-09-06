612d67a9a619736282684473612fc3862ceed8ed
Diego Reyes <diegorele13@gmail.com>
2023-09-06 12:49:58 -0500

    done: menu

---
+++ src/components/web/layout/WebMenu.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+const buttons = [
+  {
+    text: 'Botón'
+  },
+  {
+    text: 'Botón'
+  },
+  {
+    text: 'Botón'
+  },
+  {
+    text: 'Botón'
+  },
+]
+</script>
+
+<template>
+  <div class="section">
+      <button 
+        class="section-button"
+        v-for="(button, index) in buttons"
+        :key="index"
+      >
+        {{ button.text }}
+      </button>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.section {
+  display: flex;
+  flex-direction: column;
+}
+</style>
