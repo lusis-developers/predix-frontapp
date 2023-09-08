525bedc0afb992f0ee0dc9607413876ba49ec8db
Diego Reyes <diegorele13@gmail.com>
2023-09-08 12:23:00 -0500

    set: call to action

--- src/views/Web/index.vue
+++ src/views/Web/index.vue
@@ -1,18 +1,29 @@
 <script setup lang="ts">
+import CallToAction from './components/CallToAction.vue';
 import TheHero from './components/TheHero.vue';
+
+const descriptions = [
+  {
+    text: 'Pronosticamos por ti'
+  },
+  {
+    text: 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur maemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur ma'
+  },
+]
 </script>
 
 <template>
-  <div class="container-hero">
+  <div class="hero">
     <TheHero/>
   </div>
+  <CallToAction 
+    :descriptions="descriptions" 
+    button="Empezar a ganar"
+    image="/src/assets/Comments.png"/>
 </template>
 
 <style lang="scss" scoepd>
-.container {
-  &-hero {
-    display: flex;
-    justify-content: center;
-  }
+.hero {
+  margin: 0 auto;
 }
 </style>
