a0ff3a537af12e6d268056767ff48e57b76e58e1
Diego Reyes <diegorele13@gmail.com>
2023-09-08 18:12:02 -0500

    set: create account component

--- src/views/Web/index.vue
+++ src/views/Web/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import BetWay from './components/BetWay.vue';
 import CallToAction from './components/CallToAction.vue';
+import CreateCount from './components/CreateCount.vue';
 import TheHero from './components/TheHero.vue';
 
 const descriptions = [
@@ -27,6 +28,7 @@ const descriptions = [
     button="Empezar a ganar"
     image="/src/assets/historial.svg"
     :reverse="true"/>
+  <CreateCount/>
 </template>
 
 <style lang="scss" scoepd>
