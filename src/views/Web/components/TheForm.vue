ddb29171641d09de4e37ed341b272fe804ea3b0e
Diego Reyes <diegorele13@gmail.com>
2023-09-08 19:34:50 -0500

    done: form

--- src/views/Web/components/TheForm.vue
+++ src/views/Web/components/TheForm.vue
@@ -1,3 +1,57 @@
+<script setup lang="ts">
+import { computed, reactive } from 'vue';
+
+import emailjs from 'emailjs-com';
+
+
+const form = reactive({
+  name: '',
+  email: '',
+})
+
+const rules = {
+  validateName: [
+    {
+      validate: (value: string) => value.length >= 10,
+      message: 'Por favor, coloca tu nombre completo'
+    }
+  ],
+  validateEmail: [
+    {
+      validate: (value: string) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value),
+      message: 'Por favor, ingresa un correo válido'
+    }
+  ]
+}
+
+const formIsValid = computed(() => {
+  return (
+    form.name !== '' &&
+    form.email !== '' &&
+    rules.validateName.every(rule => rule.validate(form.name)) &&
+    rules.validateEmail.every(rule => rule.validate(form.email))
+  )
+});
+
+async function sendEmail() {
+  const mail = {
+    from_name: form.name,
+    from_email: form.email
+  }
+  try {
+    await emailjs.send(
+      import.meta.env.VITE_SERVICE_ID,
+      import.meta.env.VITE_TEMPLATE_ID,
+      mail,
+      import.meta.env.VITE_PUBLIC_KEY,
+    );
+    alert ('Correo enviado')
+  } catch (e) {
+    console.log('CANNOT_SEND_EMAIL:', e)
+  }
+}
+</script>
+
 <template>
   <div class="container">
     <div class="container-section">
@@ -11,17 +65,23 @@
       </div>
       <form class="container-section-form">
         <CrushTextField
+          v-model="form.name"
           class="container-section-form-input"
           label="Nombre"
+          :valid-rules="rules.validateName"
           placeholder="Roberto Moncayo"/>
         <CrushTextField
+          v-model="form.email"
           class="container-section-form-input"
           label="Ingresa tu correo"
-          placeholder="ejemplo@hola.com"/>
+          placeholder="ejemplo@hola.com"
+          :valid-rules="rules.validateEmail"/>
         <CrushButton
           class="container-section-form-button"
           variant="primary"
-          text="Enviar"/>
+          text="Enviar"
+          :disabled="!formIsValid"
+          @click.prevent="sendEmail"/>
       </form>
     </div>
   </div>
@@ -29,13 +89,14 @@
 
 <style lang="scss" scoped>
 .container {
+  padding: 12px;
   width: 100%;
   display: flex;
   &-section {
     padding: 24px;
     border-radius: 48px;
     margin: 0 auto;
-    width: 80%;
+    width: 100%;
     background: linear-gradient(270deg, #070E21 0.04%, #01DF71 267.25%);
     display: flex;
     flex-wrap: wrap;
