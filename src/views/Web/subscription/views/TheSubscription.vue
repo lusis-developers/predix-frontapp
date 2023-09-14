4b07a6f7440d01797f4c08570d85730f58c1954b
Diego Reyes <diegorele13@gmail.com>
2023-09-14 18:11:58 -0500

    done: sub page

---
+++ src/views/Web/subscription/views/TheSubscription.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import usePlansStore from '@/stores/PlanStore'
+</script>
+
+<template>
+  <div>
+    <p>
+      las suscripcion enfocada se ve aqui
+    </p>
+  </div>
+</template>
