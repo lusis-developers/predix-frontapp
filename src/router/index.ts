9be2895b72476a3b5f99cb4ba8536904bc037e57
Luis Alberto Reyes Lema <lualreye@Luiss-MacBook-Pro.local>
2023-09-22 21:48:08 -0500

    FIX: import subscription

--- src/router/index.ts
+++ src/router/index.ts
@@ -13,7 +13,7 @@ const WebContainer = () => import('@/components/web/layout/WebContainer.vue')
 //import web views
 import WebView from '@/views/Web/WebIndex.vue';
 import WebContact from '@/views/Web/TheContact.vue';
-import WebSubscriptions from '@/views/Web/Subscription/TheSubscriptions.vue';
+const WebSubscriptions = () => import('@/views/Web/Subscription/TheSubscriptions.vue');
 const PaymentDone = () => import('@/views/PaymentDone.vue');
 
 // internal app views
