a3bc05e94918bdab1cc3ff6b2007cbf5696254da
Diego Reyes <diegorele13@gmail.com>
2023-09-22 20:37:23 -0500

    set: function without upload image

--- src/views/user/Profile/TheProfile.vue
+++ src/views/user/Profile/TheProfile.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { computed, reactive, ref } from 'vue';
+
 import { useUserStore } from '@/stores/UserStore';
+import type { User } from '@/typings/UserTypes';
 
 const imageUrl = ref<string>(''); // TODO: read the image url
 const imageFile = ref<File>(new File([], '')); // TODO: store the image file
@@ -14,6 +16,13 @@ const profile = reactive({
   img: ''
 });
 
+const formIsValid = computed(() => {
+  return (
+    profile.name !== '' &&
+    profile.lastname !== '' 
+  )
+})
+
 function handleInput(event: string, type: string): void {
   if (type === 'name') {
     profile.name = event;
@@ -26,82 +35,31 @@ function handleInput(event: string, type: string): void {
 function handleFileSelected(file: File) {
   imageUrl.value = URL.createObjectURL(file);
   imageFile.value = file;
+  console.log('imageFile:', imageFile.value)
 }
 
-// function validButton {
-//   if(profile)
-// }
+async function submitUser(): Promise<void> {
+  const imageResponse = await submitImage();
+  const url = !imageResponse.length ? imageUrl.value : imageResponse;
+  const data: User = {
+    name: profile.name,
+    lastname: profile.lastname,
+    userImage: url
+  };
+  console.log(data)
+  // await userStore.updateUser(data); 
+}
 
-// async function submitImage(): Promise<string> {
-//   if (imageFile.value.size !== 0) {
-//     const result = await userStore.uploadLeagueImage(imageFile.value!);
+async function submitImage(): Promise<string> {
+  if (imageFile.value.size !== 0) {
+    const result = await userStore.uploadUserImage(imageFile.value!);
 
-//     return result?.url!;
-//   }
-//   return '';
-// }
-
-// async function prepareData(): Promise<League> {
-//   const imageResponse = await submitImage();
-//   const url = !imageResponse.length ? imageUrl.value : imageResponse;
-//   let league: League; // Inicializa como undefined
-//   let sport: string;
-
-//   if (isCreating.value) {
-//     sport = sportStore.selectedSport?._id!;
-//   } else {
-//     league = sportStore.selectedSport?.leaguesDetails?.find(league => league._id === props.leagueId)!;
-//     sport = league?.sport!;
-//   }
-
-//   let data: League = {
-//     name: name.value,
-//     image: url,
-//     sport: sport, // Sport assigned
-//   };
-
-//   return data;
-// }
-
-// async function submitLeague(): Promise<void> {
-//   const data = await prepareData();
-//   if (isCreating.value) {
-//     leagueStore.createLeague(data);
-//   } else {
-//     leagueStore.updateLeague(props.leagueId, data)
-//   }
-//   closeEdit();
-// }
-
-// function setData(): void {
-//   const league = sportStore.selectedSport?.leaguesDetails?.find(league => league._id === props.leagueId);
-//   name.value = league?.name!;
-//   imageUrl.value = league?.image!;
-// }
-
-// function resetValue(): void {
-//   name.value = '';
-//   imageFile.value = new File([], '');
-//   imageUrl.value = ''
-// }
-
-// function closeEdit() {
-//   resetValue();
-//   emit('close-create-or-edit')
-// }
-
-async function handleSave(): Promise<void> {
-  await userStore.updateUser(profile.name, profile.lastname, imageFile.value);
+    return result?.url!;
+  }
+  return '';
 }
 
 
-
-const formIsValid = computed(() => {
-  return (
-    profile.name !== '' &&
-    profile.lastname !== '' 
-  )
-})
 
 </script>
 
@@ -135,7 +93,7 @@ const formIsValid = computed(() => {
         :disabled="!formIsValid"
         variant="primary"
         text="Guardar"
-        @click="handleSave"/>
+        @click="submitUser"/>
     </div>
   </div>
 </template>
