ceb188af9670416fdecddcf81424c8b89162456d
lualreye <66281579+lualreye@users.noreply.github.com>
2024-01-03 11:25:46 -0500

    FIX: debugging google tag (#27)
    Co-authored-by: Luis Alberto Reyes Lema <lualreye@Luiss-MacBook-Pro.local>

--- src/scripts/GoogleTags.ts
+++ src/scripts/GoogleTags.ts
@@ -1,17 +1,23 @@
 const googleScript = 'https://www.googletagmanager.com/gtag/js?id=G-CMXRL7FL51';
 
-function initializeGoogleAnalytics() {
-  window.dataLayer = window.dataLayer || [];
-  function gtag(...args: any) {
-    window.dataLayer.push(args);
-  }
-  gtag('js', new Date());
-  gtag('config', 'G-CMXRL7FL51');
+async function initializeGoogleAnalytics() {
+  try {
+    window.dataLayer = window.dataLayer || [];
 
-  const script = document.createElement('script');
-  script.async = true;
-  script.src = googleScript;
-  document.head.appendChild(script);
+    gtag('js', new Date());
+    gtag('config', 'G-CMXRL7FL51');
+  
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = googleScript;
+    document.head.appendChild(script);
+  } catch (error) {
+    console.error('Google Analytincs Initalization error: ', error);
+  }
+}
+
+function gtag(...args: any) {
+  window.dataLayer.push(args);
 }
 
 export default initializeGoogleAnalytics;
